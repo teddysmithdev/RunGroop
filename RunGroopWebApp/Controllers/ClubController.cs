27b8bb4df91d383d1e5536b8259936ad73409fd9
teddysmithdev <teddysmithdev@gmail.com>
2022-02-17 21:20:47 -0500

    Added Controllers & Views

---
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index() 
+        {
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+    }
+}
---
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Race> races = _context.Races.ToList();
+            return View(races);
+        }
+    }
+}
