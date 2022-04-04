733211bead30ca28061aef96b6388a9201dfb54c
teddysmithdev <teddysmithdev@gmail.com>
2022-04-04 07:41:15 -0400

    Added unit tests for controllers

---
+++ RunGroopWebApp.Tests/Controller/ClubControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Controllers;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RunGroopWebApp.Tests.Controller
+{
+    public class ClubControllerTests
+    {
+        private ClubController _clubController;
+        private IClubRepository _clubRepository;
+        private IPhotoService _photoService;
+        private IHttpContextAccessor _httpContextAccessor;
+        public ClubControllerTests()
+        {
+            //Dependencies
+            _clubRepository = A.Fake<IClubRepository>();
+            _photoService = A.Fake<IPhotoService>();
+            _httpContextAccessor = A.Fake<HttpContextAccessor>();
+
+            //SUT
+            _clubController = new ClubController(_clubRepository, _photoService, _httpContextAccessor);
+        }
+
+        [Fact]
+        public void ClubController_Index_ReturnsSuccess()
+        {
+            //Arrange - What do i need to bring in?
+            var clubs = A.Fake<IEnumerable<Club>>();
+            A.CallTo(() => _clubRepository.GetAll()).Returns(clubs);
+            //Act
+            var result = _clubController.Index();
+            //Assert - Object check actions
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void ClubController_Detail_ReturnsSuccess()
+        {
+            //Arrange
+            var id = 1;
+            var club = A.Fake<Club>();
+            A.CallTo(() => _clubRepository.GetByIdAsync(id)).Returns(club);
+            //Act
+            var result = _clubController.Detail(id);
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+
+    }
+}
