fe45a60dec152b3b0d8e7fb5d6b6dac623108ca5
Liam <Liam.Hurley2@students.ittralee.ie>
2022-05-04 16:06:51 +0100

    Added tests for GetAll, Delete, GetCountAsync, GetAllStates and GetClubsByState methods.
    Only created test for successful queries so far, plan to create tests to check error handling/status codes at a later date.

--- RunGroopWebApp.Tests/Repository/ClubRepositoryTests.cs
+++ RunGroopWebApp.Tests/Repository/ClubRepositoryTests.cs
@@ -89,6 +89,123 @@ namespace RunGroopWebApp.Tests.Repository
             result.Should().BeOfType<Task<Club>>();
         }
 
+        [Fact]
+        public async void ClubRepository_GetAll_ReturnsList()
+        {
+            //Arrange
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
 
+            //Act
+            var result = await clubRepository.GetAll();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<List<Club>>();
+        }
+
+        [Fact]
+        public async void ClubRepository_SuccessfulDelete_ReturnsTrue()
+        {
+            //Arrange
+            var club = new Club()
+            {
+                Title = "Running Club 1",
+                Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                Description = "This is the description of the first cinema",
+                ClubCategory = ClubCategory.City,
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Charlotte",
+                    State = "NC"
+                }
+            };
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+            //Act
+            clubRepository.Add(club);
+            var result = clubRepository.Delete(club);
+            var count = await clubRepository.GetCountAsync();
+
+            //Assert
+            result.Should().BeTrue();
+            count.Should().Be(0);
+        }
+
+        [Fact]
+        public async void ClubRepository_GetCountAsync_ReturnsInt()
+        {
+            //Arrange
+            var club = new Club()
+            {
+                Title = "Running Club 1",
+                Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                Description = "This is the description of the first cinema",
+                ClubCategory = ClubCategory.City,
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Charlotte",
+                    State = "NC"
+                }
+            };
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+            //Act
+            clubRepository.Add(club);
+            var result = await clubRepository.GetCountAsync();
+
+            //Assert
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public async void ClubRepository_GetAllStates_ReturnsList()
+        {
+            //Arrange
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+            //Act
+            var result = await clubRepository.GetAllStates();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<List<State>>();
+        }
+
+        [Fact]
+        public async void ClubRepository_GetClubsByState_ReturnsList()
+        {
+            //Arrange
+            var state = "NC";
+            var club = new Club()
+            {
+                Title = "Running Club 1",
+                Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                Description = "This is the description of the first cinema",
+                ClubCategory = ClubCategory.City,
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Charlotte",
+                    State = "NC"
+                }
+            };
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+            //Act
+            clubRepository.Add(club);
+            var result = await clubRepository.GetClubsByState(state);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<List<Club>>();
+            result.First().Title.Should().Be("Running Club 1");
+        }
     }
 }
